38876b62d9e14cd5c05532acbfaf9a2c3fee744e
Nick Spragg <nick.spragg@bbc.co.uk>
2017-06-29 15:42:25 +0100

    Task: Add index

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-const Client = require('./lib/client');
-const DEFAULT_TRANSPORT = require('./lib/transport/request');
-
-module.exports.createClient = (httpTransport) => {
-  return new Client(httpTransport || DEFAULT_TRANSPORT);
-};
+module.exports = require('./lib/wreckTransport');
